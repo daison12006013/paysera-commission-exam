e2ac4b0490cccbd353e1ec301440b0b69ceb32e1
Daison Cariño <daison12006013@gmail.com>
2019-12-09 09:33:12 +0800

    Change old class name

--- bootstrap.php
+++ bootstrap.php
@@ -7,6 +7,6 @@ use Daison\Paysera\Parsers\Csv;
 
 require __DIR__ . '/vendor/autoload.php';
 
-$collections = Application::make() // or: Manager::getInstance()
+$collections = Application::make() // or: Application::getInstance()
     ->setData(new Csv(__DIR__ . '/tests/input.csv'))
     ->handle();
